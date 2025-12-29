967b0a78fab402bcc625128109698449032ddec3
acepilot950-creator <acepilot950@gmail.com>
2025-12-29 16:33:46 +0400

    initial commit

---
+++ python-classes/0-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module defines a Square class."""
+
+
+class Square:
+    """This class defines a square with a private size attribute."""
+
+    def __init__(self, size):
+        """Initialize a Square instance with a given size."""
+        self.__size = size
---
+++ python-classes/README.md
@@ -0,0 +1,3 @@
+# Python - Classes and Objects
+
+This project contains tasks for practicing OOP in Python, including creating classes with private attributes and basic methods.
