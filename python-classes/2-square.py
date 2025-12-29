32b36e93cfdaa8919d1660303a9b3b3049ed99be
acepilot950-creator <acepilot950@gmail.com>
2025-12-29 16:36:57 +0400

    initial commit

---
+++ python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""This module defines a Square class with size validation and area computation."""
+
+
+class Square:
+    """This class defines a square with a private size attribute."""
+
+    def __init__(self, size=0):
+        """Initialize a Square instance with an optional size."""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Return the current area of the square."""
+        return self.__size ** 2
